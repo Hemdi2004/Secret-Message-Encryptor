ff360e1a0781cfe4a95fd6bf99c165b5510465e4
Hemdi2004 <khamalismail2002@gmail.com>
2025-04-26 22:45:27 +0300

    Add viewMessages page, connect PHP to database, small layout improvements

---
+++ viewMessages.php
@@ -0,0 +1,43 @@
+<?php
+
+// Database connection
+
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $db_name = "secret_vault";
+
+// create connection
+
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+//chech connection
+
+if ($conn->connect_error) {
+    die("connection failed: " . $conn->connect_error);
+}
+
+                  // This will display each encrypted message in the database along with a Decrypt button next to it.
+
+//Fetch messages from DB 
+$sql = "SELECT id , context FROM messages";
+$result = $conn->query($sql);
+
+//check if there's any saved messages 
+if (result->num_rows > 0) {
+   while ($row = result->fetch_assoc()) {
+    echo "<div class='message'>"
+    echo "<p>Encrypted Message : " . htmlspecialchars($row['encryptedMessage']) . "</p>";
+
+    //Add the "Decrypt Button" (we'll handle decryption next)
+    echo "<a href='decryptedMessage.php?id=" . $row[ . 'id'] . " ' class='btn btn-primary'>Decrypt</a>";
+    echo "</div><hr>"
+
+   }
+}else {
+    echo "No messages found."
+}
+
+$conn_close();
+
+?>
